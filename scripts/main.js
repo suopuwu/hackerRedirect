d270b979a44d0ee5471c1c39c3b4acd2f35554cc
uL-hpesoJ <Josephlu8@gmail.com>
2017-02-09 21:01:17 -0800

    V1
    It's just a webpage that redirects to a link with some jokes I put in.

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {//TODO add support for choosing the presentation num
+    var data = $.getJSON('json/links.json', (function () {
+        data = data.responseJSON;
+        var link = data.links[data.links.length - 1];
+        var messages = data.messages;
+        var url = link.link;
+        //the ids of the messages to be used
+        var messageNums = link.messages;
+        //translates message numbers into ints
+        $.each(messageNums, function (index, val) {
+            messageNums[index] = parseInt(val);
+        });
+        //filters out the messages that are not needed by setting to null
+        $.each(messages, function (index, value) {
+            var isOne = false;
+            $.each(messageNums, function (idx, val) {
+                if(val === index) isOne = true;});
+            if(isOne === false) messages[index] = null;
+        });
+        //removes null messages
+        $.each(messages, function (index, val) {
+            if(val === null) {messages.splice(index, 1);}});
+        $('#bar').css('animation-duration', link.duration / messages.length + 's');
+        function aaa() {
+            $('#bar').addClass('bar-anim');
+        }
+        function changeMessage(message) {
+            $('#text').html(message);
+            $('#bar').removeClass('bar-anim');
+            setTimeout(aaa, 10);
+        }
+        //changes the message according to time
+        $.each(messages, function (index, val) {
+            setTimeout(changeMessage, (index) * ((link.duration * 1000) / messages.length), val);
+        });
+        function goToLink() {
+            window.location = url;
+        }
+        setTimeout(goToLink, link.duration * 1000);
+    }));
+});
